789740c41868665649acf78b7229bfac4465aab1
Pravin Tripathi <pravinyo12@gmail.com>
2022-08-19 18:07:57 +0530

    refactored to make it readable

--- src/main/kotlin/filters/BlackAndWhiteFilter.kt
+++ src/main/kotlin/filters/BlackAndWhiteFilter.kt
@@ -7,14 +7,25 @@ import org.opencv.imgproc.Imgproc
 class BlackAndWhiteFilter {
     fun convert(input: Mat): Mat {
         val output = Mat()
-        val grayScape = Mat()
-        val normalizeGrayScaleImage = Mat()
 
-        Imgproc.cvtColor(input, grayScape, Imgproc.COLOR_BGR2GRAY)
-        val clahe = Imgproc.createCLAHE(1.0, Size(8.0, 8.0))
-        clahe.apply(grayScape, normalizeGrayScaleImage)
+        val grayScape = convertToGrayscale(input)
+        val normalizeGrayScaleImage = adaptiveHistogramEqualization(grayScape)
         Imgproc.threshold(normalizeGrayScaleImage, output, 128.0, 255.0, Imgproc.THRESH_OTSU)
+
         return output
+    }
+
+    private fun convertToGrayscale(input: Mat): Mat {
+        val grayScape = Mat()
+        Imgproc.cvtColor(input, grayScape, Imgproc.COLOR_BGR2GRAY)
+        return grayScape
+    }
+
+    private fun adaptiveHistogramEqualization(grayScape: Mat): Mat{
+        val normalizeGrayScaleImage = Mat()
+        val clahe = Imgproc.createCLAHE(2.0, Size(8.0, 8.0))
+        clahe.apply(grayScape, normalizeGrayScaleImage)
+        return normalizeGrayScaleImage
     }
 
 }
