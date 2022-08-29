296f6b38eda429eb4b65c9a9320e401623c70e8f
Pravin Tripathi <pravinyo12@gmail.com>
2022-08-29 15:33:49 +0530

    simulate undo behavior implementation

--- src/main/kotlin/Start.kt
+++ src/main/kotlin/Start.kt
@@ -1,5 +1,13 @@
+import commands.BlackAndWhiteFilterCommand
+import commands.RotateCommand
+import filters.BlackAndWhiteFilterParameters
 import org.opencv.core.Core
+import org.opencv.core.Mat
+import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgcodecs.Imgcodecs.imread
+import transformations.FixedRotationDirection
+import transformations.RotationTransformParameters
+import java.io.File
 
 object Start {
 
@@ -7,7 +15,56 @@ object Start {
     fun main(args: Array<String>) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
 
-        val m = imread("C:/Users/tripa/Downloads/image-scanner/src/main/resources/images/sample.png")
-        println(m.rows().toString()+", "+m.cols())
+        val path = File("src/main/resources").absolutePath
+        val image = imread("$path/images/sample.png")
+
+        val stateManager = StateManager()
+        val backupManager = BackupManager()
+        val imageEditor = ImageEditor(
+            image = image,
+            stateManager = stateManager,
+            backupManager = backupManager
+        )
+
+        val rotationCommand1 = RotateCommand(
+            imageEditor,
+            RotationTransformParameters.FixedDirection(FixedRotationDirection.DIRECTION_180)
+        )
+        val rotationCommand2 = RotateCommand(
+            imageEditor,
+            RotationTransformParameters.ArbitraryDirection(30.0)
+        )
+
+        val blackAndWhiteFilterCommand = BlackAndWhiteFilterCommand(
+            imageEditor,
+            BlackAndWhiteFilterParameters()
+        )
+
+        imageEditor.takeCommand(rotationCommand1)
+        saveImage("sample_1.png", imageEditor.getActiveImage())
+
+        imageEditor.takeCommand(blackAndWhiteFilterCommand)
+        saveImage("sample_2.png", imageEditor.getActiveImage())
+
+        imageEditor.takeCommand(rotationCommand2)
+        saveImage("sample_3.png", imageEditor.getActiveImage())
+
+        imageEditor.undoChanges()
+        saveImage("sample_4.png", imageEditor.getActiveImage())
+
+        imageEditor.undoChanges()
+        saveImage("sample_5.png", imageEditor.getActiveImage())
+
+        imageEditor.undoChanges()
+        saveImage("sample_6.png", imageEditor.getActiveImage())
+
+        imageEditor.undoChanges()
+        saveImage("sample_7.png", imageEditor.getActiveImage())
+    }
+
+    fun saveImage(subPath: String, image: Mat) {
+        val path = "src/main/resources"
+        val absolutePath = File(path).absolutePath
+        Imgcodecs.imwrite("$absolutePath/output/$subPath", image)
     }
 }
