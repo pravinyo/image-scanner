1164765d71b56d55a5d48b50af621610b4368734
Pravin Tripathi <pravinyo12@gmail.com>
2022-08-30 23:51:58 +0530

    added publish code in build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
     kotlin("jvm") version "1.7.0"
     java
+    id("maven-publish")
 }
 
 group = "dev.pravin"
 version = "1.0-SNAPSHOT"
+val githubProperties = Properties()
+githubProperties.load(FileInputStream(rootProject.file("github.properties")))
 
 repositories {
     mavenCentral()
@@ -14,12 +20,36 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(files("C:/Users/tripa/Downloads/opencv-3.4.16-vc14_vc15/opencv/build/java/opencv-3416.jar"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testImplementation("io.mockk:mockk:1.12.7")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks.getByName<Test>("test") {
     jvmArgs("-Djava.library.path=C:/Users/tripa/Downloads/opencv-3.4.16-vc14_vc15/opencv/build/java/x64")
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            run {
+                groupId = "$group"
+                artifactId = rootProject.name
+                version = version
+                artifact("$buildDir/libs/${rootProject.name}-$version.jar")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/pravinyo/image-scanner")
+            credentials {
+                username = githubProperties["gpr.usr"] as String? ?: System.getenv("GPR_USER")
+                password = githubProperties["gpr.key"] as String? ?: System.getenv("GPR_API_KEY")
+            }
+        }
+    }
 }
